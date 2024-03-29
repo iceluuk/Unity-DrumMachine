344955a7fa4045d7065c7bf4bfee303755428adb
iceluuk <luuk@veldwisch.com>
2024-03-29 20:29:14 +0100

    First message of all nodes is now Pan based on x position relative to camera viewport

--- UnityDrumMachine UnityProject/Assets/Script/Node.cs
+++ UnityDrumMachine UnityProject/Assets/Script/Node.cs
@@ -9,10 +9,12 @@ public class Node : MonoBehaviour
     public float[] floatMesages = {1f};
 
     OSCTransmitter transmitter;
+    Camera mainCamera;
 
     public void SetupTransmitter()
     {
         transmitter = gameObject.AddComponent<OSCTransmitter>();
+        mainCamera = FindObjectOfType<Camera>();
 
         transmitter.RemoteHost = "127.0.0.1";
         transmitter.RemotePort = 57121;
@@ -20,7 +22,18 @@ public class Node : MonoBehaviour
 
     public void SendOSCMessage()
     {
+        // Get camera edges in world space
+        Vector3 leftEdge = mainCamera.ViewportToWorldPoint(new Vector3(0, 0.5f, mainCamera.nearClipPlane));
+        Vector3 rightEdge = mainCamera.ViewportToWorldPoint(new Vector3(1, 0.5f, mainCamera.nearClipPlane));
+
+        float pan = Mathf.InverseLerp(leftEdge.x, rightEdge.x, transform.position.x);
+
+        pan = Mathf.Lerp(-1f, 1f, pan);
+        print(pan);
+
         var message = new OSCMessage(oscAddress);
+
+        message.AddValue(OSCValue.Float(pan));
 
         foreach(float floatMessage in floatMesages){
             message.AddValue(OSCValue.Float(floatMessage));
